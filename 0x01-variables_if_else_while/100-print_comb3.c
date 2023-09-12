96673e38a2f80791277de062923b415f6d91bfec
root <lizelsd249@gmail.com>
2023-09-12 13:56:36 -0700

    done

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combinations of two digits.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			if (j <= 9 && j + i != 17)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combinations of two two digits.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 100; i++)
+	{
+		for (j = i + 1; j < 100; j++)
+		{
+			putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
+			putchar(' ');
+			putchar(j / 10 + '0');
+			putchar(j % 10 + '0');
+			if (j <= 99 && j + i != 197)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
