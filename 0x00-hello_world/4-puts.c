fd8f47dd3e76936b0240d9a4112d51d968a00b2c
root <lizelsd249@gmail.com>
2023-09-12 12:32:04 -0700

    done

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+i#include <stdio.h>
+/**
+ * main -vEntry point
+ *
+ * Return: Always 0(Success)
+ *
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
