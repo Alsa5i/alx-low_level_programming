a9c23639290d1377ff15acd1e1bda44de917c54e
root <lizelsd249@gmail.com>
2023-09-19 13:31:13 -0700

    0-reset_to_98.c file is done!

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *  * reset_to_98 - a function that reset the value of n to 98.
+ *   * 
+ *    * Return: Always 0*/
+
+
+
+void reset_to_98(int *n ) {
+	   *n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+realy i need to learn
