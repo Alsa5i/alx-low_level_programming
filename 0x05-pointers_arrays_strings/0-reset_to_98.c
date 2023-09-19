e4fa08f00f8bb18d4462ba88130f89db90f73367
root <lizelsd249@gmail.com>
2023-09-19 14:14:11 -0700

    done

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
-#inclde "main.h"
+#include "main.h"
 
 /**
  * reset_to_98
  *this function point to an intger type
- *update the value of the var
+ *@:update the value of the var
  */
 void reset_to_98(int *n)
 {
