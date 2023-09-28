a8c1604c5080630c1fc4b2f4e3b14c0009b6c905
root <lizelsd249@gmail.com>
2023-09-28 13:05:15 -0700

    fun

--- 0x08-recursion/main.c
+++ 0x08-recursion/main.c
@@ -1,12 +1,12 @@
-void _puts_recursion(char *);
+#include "main.h"
 
 /**
- * main - check the code for Holberton School students.
+ * main - check the code
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	_puts_recursion("First, solve the problem. Then, write the code");
-	return (0);
+    _print_rev_recursion("\nColton Walker");
+    return (0);
 }
