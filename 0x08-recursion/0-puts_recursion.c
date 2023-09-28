29cb1d0a2552ca79180376bd8a892813e1044d8a
root <lizelsd249@gmail.com>
2023-09-28 12:16:26 -0700

    void _puts_recursion(char *s)

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,11 +1,10 @@
 #nclude "main.h"
 
 /**
- * @s: string that we want to print
  * _puts_recursion - a function that prints a string, followed by a new line
+ * @s: string.
  */
-
-void _puts_recursion(char *s) 
+void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
