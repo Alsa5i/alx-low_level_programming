6b5a3b072722846e46ecc56244c9e37a3a361160
root <lizelsd249@gmail.com>
2023-09-28 13:08:45 -0700

    this my new day

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _puts_recursion - a function that prints a string, followed by a new line.
+ * void _print_rev_recursion(char *s) - a function that prints reverse string.
  * @s: string to print.
  */
 
