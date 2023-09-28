cb1f1b054d4f67c2974e6d81331d2c458db52972
root <lizelsd249@gmail.com>
2023-09-28 15:32:42 -0700

    is_prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,7 +3,7 @@
 int I_prime(int n, int i);
 
 /**
- * is_prime_number -  a function that returns 1 if 
+ * is_prime_number - a function that returns 1 if
  * the input integer is a prime number.
  * @n: intger number.
  * Return: 1 if the input is a prime number, otherwise return 0.
@@ -32,27 +32,4 @@ int I_prime(int n, int i)
 	if (n % i == 0 && i > 0)
 		return (0);
 	return (I_prime(n, i - 1));
-}
-
-int main()
-{
-	int r;
-
-    r = is_prime_number(1);
-    printf("%d\n", r);
-    r = is_prime_number(1024);
-    printf("%d\n", r);
-    r = is_prime_number(16);
-    printf("%d\n", r);
-    r = is_prime_number(17);
-    printf("%d\n", r);
-    r = is_prime_number(25);
-    printf("%d\n", r);
-    r = is_prime_number(-1);
-    printf("%d\n", r);
-    r = is_prime_number(113);
-    printf("%d\n", r);
-    r = is_prime_number(7919);
-    printf("%d\n", r);
-    return (0);
 }
