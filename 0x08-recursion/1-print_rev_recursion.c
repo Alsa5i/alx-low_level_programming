e50a0938f553950e309f69314440ebbb2cc6719a
root <lizelsd249@gmail.com>
2023-09-28 13:13:50 -0700

    new discription

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * void _print_rev_recursion - a function that prints a string in reverse..
+ * _print_rev_recursion - a function that prints a string in reverse.
  * @s: string to print.
  */
 
