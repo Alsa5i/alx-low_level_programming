aff74059b136c1b0663a15260da383f2b903ff0e
root <lizelsd249@gmail.com>
2023-09-28 12:26:09 -0700

    vim

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * _puts_recursion - a function that prints a string, followed by a new line
- * @s: string.
+ * _puts_recursion - a function that prints a string, followed by a new line.
+ * @s: the string.
  */
+
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
