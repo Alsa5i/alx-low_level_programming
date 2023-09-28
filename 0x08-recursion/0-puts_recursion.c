d7787b381cef5e456ea2b9edfcc26350187ed02e
root <lizelsd249@gmail.com>
2023-09-28 11:57:04 -0700

    cd root/alx-low_level_programming/0x08-recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,16 +1,17 @@
-#include "main.h"
+#nclude "main.h"
 
 /**
- * s : string that we want to print
- *
- * Return: _put_recursion
+ * @s: string that we want to print
+ * _puts_recursion - a function that prints a string, followed by a new line
  */
-void _puts_recursion(char *s)
+
+void _puts_recursion(char *s) 
 {
-	if (*s != '\0')
+	if (*s == '\0')
 	{
-	/* code */
-	_puts_recursion(s + 1);
-	printf("%c", *s);
+		putchar('\n');
+		return;
 	}
+	putchar(*s);
+	_puts_recursion(s + 1);
 }
