3c2b951f84b618dc9d49b4e23f093dc01e89fa9a
root <lizelsd249@gmail.com>
2023-09-28 13:50:59 -0700

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,7 +2,7 @@
 
 /**
  * _strlen_recursion - a function that returns the length of a string.
- * @s: string to print.
+ * @s: string to calculate it's length.
  * return: length of a string.
  */
 
