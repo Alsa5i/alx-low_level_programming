ced08dbda6e72f47821cd5b66b6f3aa63932f600
root <lizelsd249@gmail.com>
2023-10-04 13:29:53 -0700

    is

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * free_grid - function that frees a 2 dimensional grid previously created
+ * free_grid - function that frees a 2 dimensional
  * @grid: pointer
  * @height: altura
  *
