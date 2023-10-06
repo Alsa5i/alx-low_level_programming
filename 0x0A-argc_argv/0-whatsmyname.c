313e54030ae8dfbb39aebbc73f7ac0896bb1845d
root <lizelsd249@gmail.com>
2023-10-06 02:32:53 -0700

    MyName_isLizel

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+ /**
+  * main - is a function that print the name,
+  * that we pass to it follo by new line.
+  * @argc: counter of the array argv.
+  * @argv: is an array cotain the argument of our programm.
+  * Return:return  always 0
+  **/
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
