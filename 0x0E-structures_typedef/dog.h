f1efc91417add55cb7f4d5c539928195208295e4
root <lizelsd249@gmail.com>
2023-10-11 07:47:06 -0700

    All is fine

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * Struct dog a structure of types.
+ *
+ * @name: name element
+ * @age: age element
+ * @owner: an owner element
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
