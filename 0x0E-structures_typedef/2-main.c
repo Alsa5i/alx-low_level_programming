6d1144f433833ce4ccc67af8f537553b76a6d9c7
root <lizelsd249@gmail.com>
2023-10-11 10:46:10 -0700

    Print Dog

--- 0x0E-structures_typedef/2-main.c
+++ 0x0E-structures_typedef/2-main.c
@@ -16,9 +16,3 @@ int main(void)
     print_dog(&my_dog);
     return (0);
 }
-julien@ubuntu:~/0x0d. structures, typedef$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 2-main.c 2-print_dog.c -o c
-julien@ubuntu:~/0x0d. structures, typedef$ ./c 
-Name: Poppy
-Age: 3.500000
-Owner: Bob
-julien
