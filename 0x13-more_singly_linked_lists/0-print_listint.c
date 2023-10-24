1ec815dc7075e848b0749e5e91a7dbe19817facd
Alsa5i <lizelsd249@gmail.com>
2023-10-24 09:35:19 -0700

    all is fine

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -4,17 +4,18 @@
  * print_listint - Write a function that prints all
  * the elements of a listint_t list
  * @h: the value must retern
+ *
  * Return: return NEXT value
  */
 size_t print_listint(const listint_t *h)
 {
-        size_t NEXT = 0;
+	size_t NEXT = 0;
 
-        while (h != NULL)
-        {
-                printf("%d\n", h->n);
-                h = h->next;
-                NEXT++;
-        }
-        return (NEXT);
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		NEXT++;
+	}
+	return (NEXT);
 }
