bd269cbad7419fb803d943a2cd64da445fec9376
Alsa5i <lizelsd249@gmail.com>
2023-10-25 09:11:11 -0700

    a;

--- 0x13-more_singly_linked_lists/0-main.c
+++ 0x13-more_singly_linked_lists/0-main.c
@@ -1,22 +1,32 @@
-#include "lists.h"
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
 
 /**
- * print_listint
- * @h: int val
- *
- * Return: return address
+ * main - check the code
+ * 
+ * Return: Always 0.
  */
-size_t print_listint(const listint_t *h)
+int main(void)
 {
-	size_t Pi = 0;
+    listint_t *head;
+    listint_t *new;
+    listint_t hello = {8, NULL};
+    size_t n;
 
-	while (h != NULL)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-		Pi++;
-	}
-
-	return (Pi);
+    head = &hello;
+    new = malloc(sizeof(listint_t));
+    if (new == NULL)
+    {
+        printf("Error\n");
+        return (1);
+    }
+    new->n = 9;
+    new->next = head;
+    head = new;
+    n = print_listint(head);
+    printf("-> %lu elements\n", n);
+    free(new);
+    return (0);
 }
